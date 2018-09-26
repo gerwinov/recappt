e7d02d150815a7ae146c5766b896a138ece19ae3
Gerwin Overeem <gerwin@foryard.nl>
2018-09-26 21:45:26 +0200

    NIeuwe code

--- pages/recipe/index.vue
+++ pages/recipe/index.vue
@@ -2,6 +2,7 @@
   <v-layout column justify-center align-center>
     <v-flex>
       <v-btn class="mb-2" primary @click="logout">Logout</v-btn>
+      <nuxt-link to="/recipe/new">nieuw recept</nuxt-link>
       <v-card>
         <v-toolbar color="cyan" dark>
           <v-toolbar-title>Recepten</v-toolbar-title>
@@ -10,12 +11,12 @@
         <v-list two-line>
           <template v-for="(recipe, index) in recipes">
             <v-list-tile
-              :key="recipe.Name"
+              :key="recipe.name"
               @click=""
             >
               <v-list-tile-content>
-                <v-list-tile-title v-html="recipe.Name"></v-list-tile-title>
-                <v-list-tile-sub-title v-html="recipe.Text"></v-list-tile-sub-title>
+                <v-list-tile-title v-html="recipe.name"></v-list-tile-title>
+                <v-list-tile-sub-title v-html="recipe.text"></v-list-tile-sub-title>
               </v-list-tile-content>
             </v-list-tile>
           </template>
@@ -31,7 +32,7 @@ import { fireDB, fireAuth } from '~/plugins/vuefire.js'
 export default {
   firebase () {
     return {
-      recipes: fireDB.ref('/Recipes')
+      recipes: fireDB.ref(`/Recipes/${this.user.uid}`)
     }
   },
 
---
+++ pages/recipe/new.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-layout>
+    <v-flex text-xs-center xs12 sm6 offset-sm3>
+      <v-text-field
+        v-model="name"
+        label="Name"
+        required
+      ></v-text-field>
+      <v-text-field
+        v-model="text"
+        label="Text"
+        required
+      ></v-text-field>
+      <v-text-field
+        v-model="rating"
+        label="Rating"
+        type="number"
+      ></v-text-field>
+      <v-text-field
+        v-model="ratingComments"
+        label="Rating comments"
+        type="text"
+      ></v-text-field>
+     <v-btn class="register mb-2" primary @click="addRecipe">Toevoegen</v-btn>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import { fireDB, fireAuth } from '~/plugins/vuefire.js'
+
+export default {
+  // firebase () {
+  //   return {
+  //     recipes: fireDB.ref('/Recipes')
+  //   }
+  // },
+
+  data () {
+    return {
+      name: '',
+      text: '',
+      rating: 0,
+      ratingComments: ''
+    }
+  },
+
+  computed: {
+    user () {
+      return fireAuth.currentUser
+    }
+  },
+
+  methods: {
+    // Move this to drawer..
+    addRecipe () {
+      fireDB.ref(`/Recipes/${this.user.uid}`).push({
+        name: this.name,
+        text: this.text,
+        rating: this.rating,
+        ratingComments: this.ratingComments
+      }, (error) => {
+        if (error) {
+          console.log(error)
+          return
+        }
+        this.$router.push('/recipe')
+      })
+    }
+  }
+}
+</script>
